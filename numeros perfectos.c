85eeb01c8044d451d6123b98d7d05c8ea99203db
miriammadrid <31215626+miriammadrid@users.noreply.github.com>
2017-08-21 21:23:18 -0300

    Add files via upload

---
+++ numeros perfectos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*Escribir un programa que dado un número n determine si es o no un 
+  número perfecto.*/
+int main() {
+	int i, num, sum=0, cont=0;
+	
+	printf("Ingrese el numero para ver si es perfecto: ");
+	scanf("%d", &num);
+      	
+	for(i=1; i<num; i++) {
+		cont=num%i;
+		if(cont==0) {
+			sum+=i;
+		}
+	}
+       	if(sum==num){
+		printf("El numero %d es perfecto.\n", num);
+	}
+	else
+	printf("El numero %d NO es perfecto.\n", num);
+	return 0;
+}
